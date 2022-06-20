59cd7387df20a1c8b1b1ff6a25c0c813726187ea
Max-MI115 <max.murphy11@gmail.com>
2022-06-20 13:57:40 -0400

    Add Contents.m

---
+++ Contents.m
@@ -0,0 +1,5 @@
+% +DEBUGGING  Package/submodule with debugging utility functions.
+%
+% Files
+%   print_error_message   - Prints error message details to command window.
+%   print_window_event_cb - Prints event callback debugging info and moves `evt` to base workspace.
